98f7754c619ff4b54171d979ae07dafb9fa5513f
adethis <adet91@gmail.com>
2021-04-29 12:59:50 +0700

    Update new added header elements

---
+++ src/elements/parts/Header.js
@@ -0,0 +1,57 @@
+import React from 'react'
+
+import Button from 'elements/Button'
+import BrandIcon from 'elements/parts/IconText'
+
+export default function Header(props) {
+
+  const getNavLinkClass = path => {
+    return props.location.pathname === path ? ' active' : ''
+  }
+
+  return (
+    <header className="spacing-sm">
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <BrandIcon />
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarTravelon"
+            aria-controls="navbarTravelon"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarTravelon">
+            <ul className="navbar-nav ml-auto">
+              <li className={`nav-item${getNavLinkClass("/")}`}>
+                <Button className="nav-link" type="link" href="/">
+                  Home
+                </Button>
+              </li>
+              <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
+                <Button className="nav-link" type="link" href="/browse-by">
+                  Browser by
+                </Button>
+              </li>
+              <li className={`nav-item${getNavLinkClass("/stories")}`}>
+                <Button className="nav-link" type="link" href="/stories">
+                  Stories
+                </Button>
+              </li>
+              <li className={`nav-item${getNavLinkClass("/agents")}`}>
+                <Button className="nav-link" type="link" href="/agents">
+                  Agents
+                </Button>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+}
